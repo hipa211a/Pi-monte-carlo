03f0ca64d217200d909dab6f2a7a5b32a53e7630
Patel <pate_hi@sp-000060l.intra.dlr.de>
2024-06-25 12:37:21 +0200

    Added a omp parallel verison to calculate the value of pi

---
+++ omp-pi.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <cmath>
+#include <omp.h>
+
+//Function to generate random numbers between m and n
+//Returns a single value of type double
+double random_double(double m, double n)
+{
+    std::random_device rd; //seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_real_distribution<> dis(m, n); // Generate random numbers between -1 and 1
+    return dis(gen);
+}
+
+int main() {
+    int niter = 1000000; 
+    int count = 0;
+
+    #pragma omp parallel
+    {
+        int local_count = 0;
+
+        #pragma omp for nowait
+        for (int i = 0; i < niter; ++i) {
+            double x = random_double(-1.0, 1.0);
+            double y = random_double(-1.0, 1.0);
+            double z = std::sqrt(x*x + y*y);
+            if (z <= 1) {
+                ++local_count;
+            }
+        }
+
+        #pragma omp atomic
+        count += local_count;
+    }
+
+    //Calculate value of Pi
+    auto Pi = 4*(count/static_cast<double>(niter));
+    
+    //Print the value
+    std::cout << "Pi = " << Pi << std::endl;
+
+    return 0;
+}
