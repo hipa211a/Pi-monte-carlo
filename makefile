b5c0657465053fb1f316cf8f9f43806992006469
hipa211a <hirav_patel@hotmail.com>
2024-06-25 15:09:54 +0200

    Added mpi parallel version for the code

--- makefile
+++ makefile
@@ -1,22 +1,27 @@
 # Compiler
 CXX = g++-11
+MPICXX = mpic++
 
 # Compiler flags
 CXXFLAGS = -std=c++20 -fopenmp -Wall -Wextra -O2
+MPICXXFLAGS = -std=c++17 -fopenmp -Wall -Wextra -O2
 
 # Source files
-SOURCES = omp-pi.cpp serial-pi.cpp
+SOURCES = omp-pi.cpp serial-pi.cpp mpi-pi.cpp
 
 # Object files
 OBJECTS = $(SOURCES:.cpp=.o)
 
 # Executables
-EXECUTABLES = omp-pi serial-pi
+EXECUTABLES = omp-pi serial-pi mpi-pi
 
 # Default target
 all: $(EXECUTABLES)
 
 # Rule for linking
+mpi-pi: mpi-pi.o
+	$(MPICXX) $(MPICXXFLAGS) -o $@ $<
+
 omp-pi: omp-pi.o
 	$(CXX) $(CXXFLAGS) -o $@ $<
 
@@ -24,7 +29,13 @@ serial-pi: serial-pi.o
 	$(CXX) $(CXXFLAGS) -o $@ $<
 
 # Rule for compiling
-%.o: %.cpp
+mpi-pi.o: mpi-pi.cpp
+	$(MPICXX) $(MPICXXFLAGS) -c $<
+
+omp-pi.o: omp-pi.cpp
+	$(CXX) $(CXXFLAGS) -c $<
+
+serial-pi.o: serial-pi.cpp
 	$(CXX) $(CXXFLAGS) -c $<
 
 # Clean up
---
+++ mpi-pi.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <mpi.h>
+
+// Function to generate random numbers between m and n
+double random_double(double m, double n, std::mt19937& gen)
+{
+    std::uniform_real_distribution<> dis(m, n);
+    return dis(gen);
+}
+
+int main(int argc, char** argv) 
+{
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int niter = 1000000; // Total number of iterations
+    int local_niter = niter / size; // Iterations per process
+    int local_count = 0;
+    int global_count = 0;
+
+    // Create a random number generator for each process
+    std::random_device rd;
+    std::mt19937 gen(rd() + rank); // Use rank to ensure different seeds
+
+    // Perform local Monte Carlo simulations
+    for (int i = 0; i < local_niter; ++i) {
+        double x = random_double(-1.0, 1.0, gen);
+        double y = random_double(-1.0, 1.0, gen);
+        double z = std::sqrt(x*x + y*y);
+        if (z <= 1) {
+            ++local_count;
+        }
+    }
+
+    // Sum up all local counts
+    MPI_Reduce(&local_count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    // Calculate and print Pi on the root process
+    if (rank == 0) {
+        double Pi = 4.0 * global_count / static_cast<double>(niter);
+        std::cout << "Pi = " << Pi << std::endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
